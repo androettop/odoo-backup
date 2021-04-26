239eb5ee2a90c762ec239e5f7783de1370b8c106
Pablo Androetto <androettop@gmail.com>
2021-04-26 02:20:25 -0300

    working with promises

--- bin/index.js
+++ bin/index.js
@@ -20,63 +20,65 @@ const driveInstalled = () => {
     return false;
   }
 };
+const main = async () => {
+  try {
+    const backupDir = getEnv("ODOO_BACKUP_DIR");
+    const database = getEnv("ODOO_DATABASE_NAME");
+    const odooUrl = getEnv("ODOO_URL");
+    const adminPassword = getEnv("ODOO_ADMIN_PASSWORD");
+    const driveDir = getEnv("ODOO_DRIVE_DIR", false);
+    const backupName = `${database}.${new Date().toISOString()}.zip`;
+    const fullFileName = `${backupDir}/${backupName}`;
 
-try {
-  const backupDir = getEnv("ODOO_BACKUP_DIR");
-  const database = getEnv("ODOO_DATABASE_NAME");
-  const odooUrl = getEnv("ODOO_URL");
-  const adminPassword = getEnv("ODOO_ADMIN_PASSWORD");
-  const driveDir = getEnv("ODOO_DRIVE_DIR", false);
-  const backupName = `${database}.${new Date().toISOString()}.zip`;
-  const fullFileName = `${backupDir}/${backupName}`;
+    // Create the backup directory
+    if (!fs.existsSync(backupDir)) {
+      fs.mkdirSync(backupDir);
+      console.log("Backup directory created");
+    }
 
-  // Create the backup directory
-  if (!fs.existsSync(backupDir)) {
-    fs.mkdirSync(backupDir);
-    console.log("Backup directory created");
-  }
+    // Download backup
+    console.log("Downloading backup...");
+    await new Promise((resolve, reject) => {
+      request
+        .post(`${odooUrl}/web/database/backup`, {
+          form: {
+            master_pwd: adminPassword,
+            name: database,
+            backup_format: "zip",
+          },
+        })
+        .on("error", function (e) {
+          reject(`ERROR: ${e.message}`);
+        })
+        .on("complete", function (err) {
+          resolve();
+        })
+        .pipe(fs.createWriteStream(fullFileName));
+    });
+    console.log("\nBackup downloaded");
 
-  // Download backup
-  console.log("Downloading backup...");
-  new Promise((resolve, reject) => {
-    request
-      .post(`${odooUrl}/web/database/backup`, {
-        form: {
-          master_pwd: adminPassword,
-          name: database,
-          backup_format: "zip",
-        },
-      })
-      .on("error", function (e) {
-        reject(`ERROR: ${e.message}`);
-      })
-      .on("complete", function (err) {
-        resolve();
-      })
-      .pipe(fs.createWriteStream(fullFileName));
-  })
-    .then(() => {
-      console.log("\nBackup downloaded")
+    // TODO: delete old backups
 
-      if (driveDir && driveInstalled()) {
-        spawnSync("drive", [
-          "push",
-          "-quiet",
-          "-destination",
-          driveDir,
-          fullFileName,
-        ]);
-      } else {
-        console.log(
-          "\nYou can upload your backups to google drive.\
+    // Upload backup to google drive
+    if (driveDir && driveInstalled()) {
+      spawnSync("drive", [
+        "push",
+        "-quiet",
+        "-destination",
+        driveDir,
+        fullFileName,
+      ]);
+    } else {
+      console.log(
+        "\nYou can upload your backups to google drive.\
         \ninstall drive using: sudo snap install drive\
         \nand sign in with: drive init"
-        );
-      }
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-} catch (e) {
-  console.log(`ERROR: ${e.message}`);
-}
+      );
+    }
+
+  } catch (e) {
+    console.log(`ERROR: ${e.message}`);
+  }
+};
+
+main();
